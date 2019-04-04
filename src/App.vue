28452c1d10b4cff4021e822266d778d0f1699b75
omar-nav <omar.navarro@g.austincc.edu>
2019-04-04 11:19:43 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template lang = "pug">
+  #app
+    input(v-model="name")
+    input(v-model="lastName")
+    p {{ fullName }}
+    input(v-model="dob" placeholder="year-month-day")
+    p {{ age }}
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      name: 'omar',
+      lastName: 'cesar',
+      dob: '1990-01-01'
+    }
+  },
+  computed: {
+    fullName () {
+      return this.name + ' ' + this.lastName
+    },
+    age () {
+      let dob = new Date(this.dob)
+      let today = new Date()
+      let age = today - dob
+      return age
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "./scss/main.scss";
+</style>
